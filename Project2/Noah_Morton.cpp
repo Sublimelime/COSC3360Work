9dd74fc1602f56d442b96263f93f590d2726f969
Noah M <noahcodes@protonmail.com>
2022-03-30 15:02:30 -0500

    Implement small beginning of first file read

--- Project2/Noah_Morton.cpp
+++ Project2/Noah_Morton.cpp
@@ -8,6 +8,31 @@
 #include <unistd.h>
 #include <vector>
 
-int main(int argc, char** argv) {
+int main(int argc, char **args) {
+  using namespace std;
+
+  if (argc != 3) {
+    printf("Started with too many/few arguments, exiting.\n");
+    return 1;
+  }
+
+  fstream inFile1, inFile2;
+  inFile1.open(args[1], ios::in);
+  if (!inFile1.is_open()) {
+    printf("Unable to open matrix input file, exiting.\n");
+    return 1;
+  }
+
+  // holds lines from `many` file, used to setup program
+  auto commandFileLines = vector<string>();
+  string line;
+
+  //read in all lines from file
+  while (inFile1) {
+    getline(inFile1, line);
+    commandFileLines.push_back(line);
+  }
+  inFile1.close();
+
 
 }
