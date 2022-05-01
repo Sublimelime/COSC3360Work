9fba110ac07e322a12d97aa86b27ea99436821e4
Noah M <noahcodes@protonmail.com>
2022-05-01 13:59:15 -0500

    Add basic parsing for initial setup ints

--- Project3/Noah_Morton.cpp
+++ Project3/Noah_Morton.cpp
@@ -11,17 +11,39 @@
 int main(int argc, char *args[]) {
   using namespace std;
 
-  if (argc != 3) {
+  if (argc != 2) {
     printf("Started with too many/few arguments, exiting.\n");
     return 1;
   }
 
-  fstream inFile1, inFile2;
-  inFile1.open(args[1], ios::in);
-  if (!inFile1.is_open()) {
+  fstream inFile;
+  inFile.open(args[1], ios::in);
+  if (!inFile.is_open()) {
     printf("Unable to open input file, exiting.\n");
     return 1;
   }
+
+  // integers keeping track of stats
+  int totalPageFrames, pageSize, pageFramesPerProc, lookaheadWindowSize,
+      minFreePoolSize, maxFreePoolSize, totalProcs;
+
+  // Read file into vector, then close
+  auto inputFileStrings = vector<string>();
+  string line;
+  while (inFile) {
+    getline(inFile, line);
+    inputFileStrings.push_back(line);
+  }
+  inFile.close();
+
+  // set statistic ints
+  totalPageFrames = stoi(inputFileStrings.at(0));
+  pageSize = stoi(inputFileStrings.at(1));
+  pageFramesPerProc = stoi(inputFileStrings.at(2));
+  lookaheadWindowSize = stoi(inputFileStrings.at(3));
+  minFreePoolSize = stoi(inputFileStrings.at(4));
+  maxFreePoolSize = stoi(inputFileStrings.at(5));
+  totalProcs = stoi(inputFileStrings.at(6));
 
   return 0;
 }
